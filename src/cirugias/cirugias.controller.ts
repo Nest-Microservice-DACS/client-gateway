27b8e6dccd22798017ba670a1bc39459030891fc
Lucas Carotta <carottalucas@gmail.com>
2025-12-31 23:03:53 +0000

    feat: enhance Cirugias module with pagination and custom exception handling, add class-transformer and class-validator dependencies

--- src/cirugias/cirugias.controller.ts
+++ src/cirugias/cirugias.controller.ts
@@ -1,22 +1,54 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { Console } from 'console';
+import { catchError, first, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
 
 @Controller('cirugias')
 export class CirugiasController {
-  constructor() {}
+  constructor(
+    @Inject('CIRUGIAS_SERVICE') private readonly cirugiasClient: ClientProxy,
+  ) {}
 
   @Post()
   createCirugia(@Body() createData: any) {
     return 'This action adds a new cirugia';
-  }   
+  }
 
   @Get()
-  getAllCirugias() {
-    return 'This action returns all cirugias';
+  getAllCirugias(@Query() paginationDto: PaginationDto) {
+    return this.cirugiasClient.send({ cmd: 'get_cirugias' }, paginationDto);
   }
 
   @Get(':id')
-  getCirugiaById(@Param('id') id: string) {
-    return 'This action returns a cirugia by id';
+  async getCirugiaById(@Param('id') id: string) {
+    // Using pipe and catchError to handle exceptions
+    return this.cirugiasClient.send({ cmd: 'get_cirugia_by_id' }, { id }).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+
+    // Alternatively, using firstValueFrom with try-catch
+    // try {
+    //   const cirugia = await firstValueFrom(
+    //     this.cirugiasClient.send({ cmd: 'get_cirugia_by_id' }, { id }),
+    //   );
+    //   return cirugia;
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
   }
 
   @Patch(':id')
